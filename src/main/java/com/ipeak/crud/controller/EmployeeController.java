f7ea7607cc554d63ef8e5725d8f157e7787f0b73
payneTsai <30742937+payneTsai@users.noreply.github.com>
2017-10-17 19:01:35 +0800

    改用json传输数据

--- src/main/java/com/ipeak/crud/controller/EmployeeController.java
+++ src/main/java/com/ipeak/crud/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -22,19 +23,28 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @RequestMapping("/emps")
-    public String getEmps(@RequestParam(value = "pn",defaultValue ="1")Integer pn,Model model){
+//    @RequestMapping("/emps")
+//    public String getEmps(@RequestParam(value = "pn",defaultValue ="1")Integer pn,Model model){
+//
+////      1.引入分页插件
+////      在查询之前调用传入页码，以及每页的大小
+//        PageHelper.startPage(pn,5);
+//        List<Employee> emps = employeeService.getAll();
+////        使用pageinfo包装，只需要将pageinfo交给页面就行了
+////        它封装了详细的分页信息，包括我们查询出来的信息,连续显示5个页数
+//        PageInfo page = new PageInfo(emps,5);
+//        model.addAttribute("pageInfo",page);
+//
+//        return "list";
+//}
 
-//      1.引入分页插件
-//      在查询之前调用传入页码，以及每页的大小
+    @RequestMapping("/emps")
+    @ResponseBody
+    public PageInfo getEmpsWithJson(@RequestParam(value = "pn",defaultValue ="1")Integer pn){
         PageHelper.startPage(pn,5);
         List<Employee> emps = employeeService.getAll();
-//        使用pageinfo包装，只需要将pageinfo交给页面就行了
-//        它封装了详细的分页信息，包括我们查询出来的信息,连续显示5个页数
         PageInfo page = new PageInfo(emps,5);
-        model.addAttribute("pageInfo",page);
-
-        return "list";
-}
+        return page;
+    }
 
 }
